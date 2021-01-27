03daed6203e306bf54b60a4591da2b1e8d34833c
hyh <1109466796@qq.com>
2021-01-27 16:51:34 +0800

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,76 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
+_loadData() async{
+  String dataUrl = '';
+  http.Response response= await http.get(dataUrl);
+
+}
+
+main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "flutter demo",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          elevation: 5,
+          shadowColor: Colors.grey,
+        ),
+        body: HomeContent(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomeContent extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Center(
+      child: Container( // div
+        child: Text("你好,这是个flutter项目的demo",
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.blue,
+            fontWeight: FontWeight.w300,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.white,
+            decorationStyle: TextDecorationStyle.dashed,
+            letterSpacing: 2.0,
+          ),
+          overflow: TextOverflow.ellipsis,
+          textScaleFactor: 1.5,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          border: Border.all(
+            color: Colors.blue,
+            width: 2
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.bottomCenter,
       ),
     );
   }
+
 }
